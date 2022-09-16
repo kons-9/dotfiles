714efba85736cd1d5d4b2dcdec18c81dedbe7d12
toshiki goto <gotoshi.euphonium@gmail.com>
2022-09-16 11:35:43 +0900

    Update

---
+++ nvim-setting/nvim/lua/colors.lua
@@ -0,0 +1,13 @@
+-- colorscheme
+-- vim.cmd [[colorscheme molokai]]
+-- vim.cmd [[colorscheme hybrid]]
+-- vim.cmd [[colorscheme iceberg]]
+vim.cmd([[colorscheme myiceberg]])
+vim.cmd([[set t_Co=256]])
+vim.cmd([[syntax enable]])
+vim.cmd([[highlight Normal ctermbg=NONE guibg=NONE]])
+vim.cmd([[highlight NonText ctermbg=NONE guibg=NONE]])
+vim.cmd([[highlight SpecialKey ctermbg=NONE guibg=NONE]])
+vim.cmd([[highlight EndOfBuffer ctermbg=NONE guibg=NONE]])
+vim.cmd([[hi LineNr ctermbg=NONE ctermfg=241 guibg=NONE guifg=#444b71]])
+vim.cmd([[hi Visual ctermfg=NONE ctermbg=240]])
