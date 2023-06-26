db9984bdeef42255634034aea1d3d909160aea53
toshiki goto <gotoshi.euphonium@gmail.com>
2023-06-26 23:41:50 +0900

    Update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,22 @@
 # git prompt
 setopt PROMPT_SUBST
 RPROMPT='%*'
-RPROMPT='$GITSTATUS_PROMPT'
 
 function __virtualenv_info {
-    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`')'
+  [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`')'
 }
 
 function __git_status_info {
   [ ${GITSTATUS_PROMPT} ] && echo '('${GITSTATUS_PROMPT}')'
+  # echo '('${GITSTATUS_PROMPT}')'
 }
 
-function precmd {
+function makeprompt {
 PROMPT="%F{green}%n@%m: %F{yellow}%~%f
 $(__virtualenv_info)$(__git_status_info)%F{white}%%%f "
+# $(__virtualenv_info)$(__git_status_info)%F{white}%%%f "
+RPROMPT='$(__git_status_info)'
 }
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd makeprompt
