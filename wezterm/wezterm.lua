9c6109533cad5e3541a46f9397faabce76e7a88e
toshiki goto <gotoshi.euphonium@gmail.com>
2023-06-27 18:36:08 +0900

    Update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,16 +5,25 @@ local wezterm = require 'wezterm';
 local os = wezterm.target_triple
 local config = {}
 
+if wezterm.config and wezterm.config.builder then
+  config = wezterm.config.builder()
+end
+
 if os:find("windows") then
   config.default_prog = {"wsl.exe", "--distribution", "Ubuntu-20.04", "--exec", "/usr/bin/zsh", "-l"}
 end
 -- macos
 if os:find("darwin") then
-  -- hoge
+  config.color_scheme = "Dracula"
 end
 -- linux
 if os:find("linux") then
   -- hoge
 end
+
+config.color_scheme = 'Hybrid (Gogh)'
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.window_background_opacity = 0.85
+config.font_size = 14.0
 
 return config
