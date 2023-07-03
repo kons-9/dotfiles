02c9b733c03a7d26eef0bb0395c5e4a001f66b97
toshiki goto <gotoshi.euphonium@gmail.com>
2023-07-03 16:51:29 +0900

    use xdg in gitconfig

--- start.zsh
+++ start.zsh
@@ -1,32 +1,41 @@
 #!/bin/env zsh
 cd `dirname $0`
 dirpath=`pwd`
+# if XDG_CONFIG_HOME is not set, set it to ~/.config
+if [ -z $XDG_CONFIG_HOME ]; then
+  export XDG_CONFIG_HOME=~/.config
+fi
 
-# if check is not needed, please cmd `sh start.sh -y`
+# if check is not needed and you want to replace all, please cmd `sh start.sh -y`
+# if check is not needed and you don't want to replace any, please cmd `sh start.sh -n`
 flag=$1 || ""
 
+#####################
+# use XDG_CONFIG_HOME
+#####################
 nvim_source="${dirpath}/nvim"
-nvim_target=~/.config/nvim
+nvim_target=$XDG_CONFIG_HOME/nvim
 
 zsh_source="${dirpath}/zsh/"
-zsh_target=~/.config/zsh
+zsh_target=$XDG_CONFIG_HOME/zsh
+
+clang_source="${dirpath}/clang/.clang-format"
+clang_target=$XDG_CONFIG_HOME/.clang-format
+
+wezterm_source="${dirpath}/wezterm/"
+wezterm_target=$XDG_CONFIG_HOME/wezterm
+
+git_config_source="${dirpath}/git/"
+git_config_target=$XDG_CONFIG_HOME/git
+
+
+vscode_keybindings_source="${dirpath}/vscode/keybindings.json"
 
 zshrc_source="${dirpath}/zsh/.zshenv"
 zshrc_target=~/.zshenv
 
 zshlocal_source="${dirpath}/zsh/.zshrc.local"
 zshlocal_target=~/.zshrc
-
-clang_source="${dirpath}/clang/.clang-format"
-clang_target=~/.clang-format
-
-wezterm_source="${dirpath}/wezterm/"
-wezterm_target=~/.config/wezterm
-
-git_config_source="${dirpath}/git/.gitconfig"
-git_config_target=~/.gitconfig
-
-vscode_keybindings_source="${dirpath}/vscode/keybindings.json"
 
 hammerspoon_source="${dirpath}/.hammerspoon"
 hammerspoon_target=~/.hammerspoon
@@ -41,10 +50,15 @@ function makeSymLink() {
   fi
 
   if [ ! -e $target ] && [ ! -L $target ]; then
+    echo "make symlink ${target}!"
     mkdir -p `dirname $target`
     ln -s $source $target
   else
     echo "${target} is exist."
+    if [ "$flag" = "-n" ]; then
+      echo "skip ${target}!"
+      return 
+    fi
     if [ ! "$flag" = "-y" ]; then
       read "YN?replace it? (y/n) :"
       if [ $YN = "n" ];then
