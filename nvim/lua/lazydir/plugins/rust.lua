19753b2330795ecfc202c468fe9648bb5dc7113c
Toshiki Goto <gotoshi.euphonium@gmail.com>
2023-07-14 22:01:44 +0900

    fix bug

--- nvim/lua/lazydir/plugins/rust.lua
+++ nvim/lua/lazydir/plugins/rust.lua
@@ -4,30 +4,9 @@ local spec = {
     event = { "BufRead Cargo.toml" },
     requires = {
       "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "jose-elias-alvarez/null-ls.nvim",
     },
     config = function()
-      local _, null_ls = utils.safe_require("null-ls")
-      require("crates").setup {
-        null_ls = {
-          enabled = true,
-          name = "crates"
-        },
-      }
-      utils.make_autocmds({
-        augroup = "Crates",
-        autocmds = {
-          event = "BufRead",
-          opts = {
-            pattern = "Cargo.toml",
-            callback = function()
-              local _, cmp = utils.safe_require("cmp")
-              cmp.setup.buffer({ sources = { { name = "crates" } } })
-            end,
-          }
-        },
-      })
+      require("crates").setup {}
     end,
   }
 
