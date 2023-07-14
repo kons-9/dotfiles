59938affbfc175df3f4945998fa67f25d7e1bd4e
Toshiki Goto <gotoshi.euphonium@gmail.com>
2023-07-14 21:55:28 +0900

    markdown preview

---
+++ nvim/lua/lazydir/plugins/markdown.lua
@@ -0,0 +1,8 @@
+local spec = {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+  ft = { "markdown" },
+  -- cmd = { "MarkdownPreview" },
+}
+return spec
