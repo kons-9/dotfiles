7efc0ec880d39a9e08a64f183239f3b1dd8a41ac
toshiki goto <gotoshi.euphonium@gmail.com>
2024-08-05 12:29:42 +0900

    nit fix of wezterm startup

--- wezterm/startup.lua
+++ wezterm/startup.lua
@@ -4,7 +4,8 @@ local function merge(config)
     local mux = wezterm.mux
     wezterm.on("gui-startup", function(cmd)
         local tab, pane, window = mux.spawn_window(cmd or {})
-        window:gui_window():toggle_fullscreen()
+        -- window:gui_window():toggle_fullscreen()
+        window:gui_window():maximize()
     end)
 end
 
